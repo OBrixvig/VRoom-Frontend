60a4231da81209c616848f706ad2068cb6dda272
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2025-05-07 11:18:58 +0200

    Har opdateret Js. med module

--- Js/FrontPage.js
+++ Js/FrontPage.js
@@ -1,6 +1,20 @@
+import { getAll, create, getById, remove, update} from './AxiosCRUD.js';
+
+const { createApp } = Vue;
+
 const app = Vue.createApp({
     data() {
         return {
+            Bookings: [],
+            newBooking: {
+                room: '',
+                date: '',
+                timeSlot: '',
+                userEmail: '',
+                isActive: true,
+                // Tænker at teamMebers er et Array af objekter, som har et StudenderId og en Email?
+                teamMembers: [],
+            },
             intro: 'Du begynder bare at lave lågsus ting her Oliver',
         }
     },
